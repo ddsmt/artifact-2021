3fa64f80391ccbb8eb101ecd9bd98350c565e59d
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2021-01-18 11:55:05 +0100

    Add overview table.

--- results.py
+++ results.py
@@ -1,8 +1,34 @@
 #!/usr/bin/env python3
 
 import glob
+import jinja2
 import os
 import sqlite3
+import subprocess
+
+
+jenv = jinja2.Environment(
+    loader=jinja2.FileSystemLoader('stuff/'),
+	variable_start_string='\VAR{',
+	variable_end_string='}',
+)
+
+
+def solver_name(s):
+    d = {
+        'ddsmt-master': 'ddsmt',
+        'ddsmt-dev-ddmin': 'ddmin',
+        'ddsmt-dev-ddmin-j1': 'ddmin-j1',
+        'ddsmt-dev-naive': 'ddnaive',
+        'ddsmt-dev-naive-j1': 'ddnaive-j1',
+    }
+    return d.get(s, s)
+
+
+def render_to_file(filename, tplfile, **kwargs):
+    with open(filename, 'w') as file:
+        tpl = jenv.get_template(tplfile)
+        file.write(tpl.render(**kwargs))
 
 
 def setup_database():
@@ -47,11 +73,55 @@ def load_solver(inputs, solver):
         add_result(filename, solver, insize, outsize, runtime)
 
 
+def get_all_results():
+    cur = db.execute('''
+SELECT input, solver, insize, outsize, time FROM data
+''')
+    data = {}
+    solvers = []
+    inputs = []
+    for c in cur:
+        if c['input'] not in data:
+            data[c['input']] = {'insize': c['insize']}
+        assert c['insize'] == data[c['input']]['insize']
+        data[c['input']][solver_name(c['solver'])] = {
+            'outsize': c['outsize'],
+            'time': c['time'],
+        }
+        inputs.append(c['input'])
+        solvers.append(solver_name(c['solver']))
+    for i in data:
+        print(data[i])
+        best = data[i]['insize']
+        for s in data[i]:
+            if s == 'insize':
+                continue
+            best = min(best, data[i][s]['outsize'])
+        for s in data[i]:
+            if s == 'insize':
+                continue
+            if data[i][s]['outsize'] == best:
+                data[i][s]['best'] = True
+    return {
+        'data': data,
+        'inputs': sorted(list(set(inputs))),
+        'solvers': sorted(list(set(solvers))),
+    }
+
+
+def do_analysis():
+    render_to_file('out/table.tex', 'table-overview.tpl', **get_all_results())
+    subprocess.run(['pdflatex', 'table.tex'], cwd='out/')
+
+
 if os.path.isfile('db.db'):
     os.unlink('db.db')
 db = sqlite3.connect('db.db')
+db.row_factory = sqlite3.Row
 setup_database()
 
 inputs = load_inputs()
 for solver in os.listdir('out/'):
     load_solver(inputs, solver)
+
+do_analysis()
